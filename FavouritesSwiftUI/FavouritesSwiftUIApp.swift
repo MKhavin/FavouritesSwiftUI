7e5a05ed09338b9d57287d09b081ca4d8d36c5a6
MichaelK <havinmichael@gmail.com>
2022-08-10 22:42:18 +0300

    Initial Commit

---
+++ FavouritesSwiftUI/FavouritesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritesSwiftUIApp.swift
+//  FavouritesSwiftUI
+//
+//  Created by Michael Khavin on 10.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct FavouritesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
